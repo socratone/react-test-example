3692bb5732620ce1bac81d78a13625e2314800cd
socratone <gim2origin@gmail.com>
2021-11-09 02:37:25 +0900

    fix: async에 mock fn 적용

--- src/pages/Async/Async.test.jsx
+++ src/pages/Async/Async.test.jsx
@@ -1,8 +1,15 @@
+import axios from 'axios';
 import { render, screen } from '@testing-library/react';
 import Async from './index';
 
+jest.mock('axios');
+
 describe('<Async />', () => {
   test('데이터를 불러와야 한다.', async () => {
+    const users = [{ id: 1, name: 'socratone' }];
+    const res = { data: users };
+    axios.get.mockResolvedValue(res);
+
     render(<Async />);
     const listItemElements = await screen.findAllByRole('listitem'); // 찾을 때까지 기다렸다가 실행된다. 기본값 1초
     expect(listItemElements).not.toHaveLength(0);
