42b76a5c8232681342a7a42237245254ab9026e7
socratone <gim2origin@gmail.com>
2021-12-08 21:58:48 +0900

    feat: event 테스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Basic from './pages/Basic';
 import MainNav from './components/layout/MainNav';
 import MainNavItem from './components/layout/MainNavItem';
 import Async from './pages/Async';
+import Event from './pages/Event';
 
 function App() {
   return (
@@ -10,9 +11,11 @@ function App() {
       <MainNav>
         <MainNavItem to="/basic">Basic</MainNavItem>
         <MainNavItem to="/async">Async</MainNavItem>
+        <MainNavItem to="/event">Event</MainNavItem>
       </MainNav>
       <Routes>
         <Route path="/async" element={<Async />} />
+        <Route path="/event" element={<Event />} />
         <Route path="*" element={<Basic />} />
       </Routes>
     </div>
---
+++ src/pages/Event/Event.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Event from '.';
+
+describe('<Event />', () => {
+  // click event
+  test('버튼을 클릭하면 1이 입력돼야 한다.', () => {
+    render(<Event />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const textElement = screen.getByText('1');
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // type event
+  test('input에 123456789를 입력하더라도 12345가 나와야 한다.', () => {
+    render(<Event />);
+
+    const inputElement = screen.getByRole('textbox') as HTMLInputElement;
+    userEvent.type(inputElement, '123456789');
+
+    expect(inputElement.value).toBe('12345');
+  });
+});
