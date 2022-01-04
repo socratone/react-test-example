3d0358e7ecfec07c9d44386006ebaf1f5b9938bc
socratone <gim2origin@gmail.com>
2022-01-04 22:20:42 +0900

    fix: role 추가

--- src/tests/role.test.tsx
+++ src/tests/role.test.tsx
@@ -22,4 +22,48 @@ describe('Role로 Element를 찾는 방법', () => {
     const element = screen.getByRole('button');
     expect(element).toBeInTheDocument();
   });
+
+  test('radio를 찾는다.', () => {
+    render(
+      <div>
+        <input type="radio" />
+      </div>
+    );
+
+    const element = screen.getByRole('radio');
+    expect(element).toBeInTheDocument();
+  });
+
+  test('checkbox를 찾는다.', () => {
+    render(
+      <div>
+        <input type="checkbox" />
+      </div>
+    );
+
+    const element = screen.getByRole('checkbox');
+    expect(element).toBeInTheDocument();
+  });
+
+  test('img를 찾는다.', () => {
+    render(
+      <div>
+        <img alt="" />
+      </div>
+    );
+
+    const element = screen.getByRole('img');
+    expect(element).toBeInTheDocument();
+  });
+
+  test('article을 찾는다.', () => {
+    render(
+      <div>
+        <article />
+      </div>
+    );
+
+    const element = screen.getByRole('article');
+    expect(element).toBeInTheDocument();
+  });
 });
