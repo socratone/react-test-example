bead755f9413f130903a1691784f3dcb346a28fa
socratone <gim2origin@gmail.com>
2022-01-05 01:51:51 +0900

    fix: 불필요한 test용 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import Basic from './pages/Basic';
 import MainNav from './components/layout/MainNav';
 import MainNavItem from './components/layout/MainNavItem';
 import Async from './pages/Async';
-import Event from './pages/Event';
-import BlurEvent from './pages/BlurEvent';
 
 function App() {
   return (
@@ -12,13 +10,9 @@ function App() {
       <MainNav>
         <MainNavItem to="/basic">Basic</MainNavItem>
         <MainNavItem to="/async">Async</MainNavItem>
-        <MainNavItem to="/event">Event</MainNavItem>
-        <MainNavItem to="/blur-event">BlurEvent</MainNavItem>
       </MainNav>
       <Routes>
         <Route path="/async" element={<Async />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/blur-event" element={<BlurEvent />} />
         <Route path="*" element={<Basic />} />
       </Routes>
     </div>
