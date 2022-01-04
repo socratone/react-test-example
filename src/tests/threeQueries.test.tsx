5f1ad0293cd030ef5f82d45929964dd5fd974efc
socratone <gim2origin@gmail.com>
2022-01-04 21:54:23 +0900

    feat: 3가지 query 방식 사용법 추가

---
+++ src/tests/threeQueries.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+
+describe('3가지 Query를 쓰는 방법', () => {
+  test(`getBy`, () => {
+    render(
+      <div>
+        <button />
+      </div>
+    );
+
+    const element = screen.getByRole('button'); // 못찾으면 에러 발생
+    expect(element).toBeInTheDocument();
+  });
+
+  test(`queryBy`, () => {
+    render(
+      <div>
+        <button />
+      </div>
+    );
+
+    const element = screen.queryByRole('textbox'); // 몾찾으면 null return
+    expect(element).toBe(null);
+  });
+
+  test(`findBy`, async () => {
+    render(
+      <div>
+        <button />
+      </div>
+    );
+
+    const element = await screen.findByRole('button'); // promise를 return
+    expect(element).toBeInTheDocument();
+  });
+});
